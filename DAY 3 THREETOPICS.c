9347bef1bb6aa142c53aaf6a8ec83e4762bf2c61
Pushpa-ux-spac <pst8269608148pt146@gmail.com>
2024-10-01 17:39:45 +0530

    Add files via upload

---
+++ DAY 3 THREETOPICS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	
+        int a,b,c,x;
+        scanf("%d %d %d %d",&a,&b,&c,&x);
+        if(x==a || x==b || x==c)
+        printf("YES \n");
+        else
+        printf("NO \n");
+       
+        
+	// your code goes here
+	return 0;
+}
+
+
+
