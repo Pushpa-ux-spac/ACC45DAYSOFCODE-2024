a09c4019feb17d0ba47271563ea145c28ad83eaa
Pushpa-ux-spac <pst8269608148pt146@gmail.com>
2024-10-04 18:29:05 +0530

    Add files via upload

---
+++ DAY 7 JASSIGNMENTS.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	
+	
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x<=7)
+	    {
+	        printf("\nyes");
+	    }
+	    else
+	    {
+	        printf("\nno");
+	    }
+	}
+	return 0;
+}
+
+
