5ec86d5e367ca327bebab555f3ffb401ef92a0f3
Pushpa-ux-spac <pst8269608148pt146@gmail.com>
2024-10-08 19:31:36 +0530

    Add files via upload

---
+++ DAY 11 POLTHIEF.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+        if(x>y){
+            printf("%d\n",x-y);
+        }else{
+            printf("%d\n",y-x);
+        }
+    }
+	// your code goes here
+	return 0;
+}
+
+
+
