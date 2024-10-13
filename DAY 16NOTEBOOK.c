a964d0cd263ce4f12fc58f1e288c18f23594c86a
Pushpa-ux-spac <pst8269608148pt146@gmail.com>
2024-10-13 18:16:45 +0530

    Add files via upload

---
+++ DAY 16NOTEBOOK.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+
+int main(void) 
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int n,a;
+	    scanf("%d",&n);
+	    a=10*n;
+	    printf("\n%d",a);
+	}
+	return 0;
+}
+
+
+
+
